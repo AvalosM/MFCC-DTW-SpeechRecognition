231b522b00248bec4a2b5b44d74c733b61a4168e
AvalosM <manuel.avalos.98@gmail.com>
2022-04-29 12:47:46 -0300

    Asm vector functions

--- src/vector.asm
+++ src/vector.asm
@@ -49,7 +49,99 @@ vectorfc_dot_asm:
 
     .end:
     movaps xmm0, sum
-    ; ----------------
-    ; ----Epilogue----
-    ; ----------------
+
     ret
+
+global vectorf_dot_asm
+;
+;
+vectorf_dot_asm:
+    ; ----------------
+    ; ------Body------
+    ; ----------------
+    %define x rdi
+    %define y rsi
+
+    %define length r8
+    mov length, rdx
+    sub length, 3
+
+    %define i rcx
+    mov i, 0
+    
+    %define sum, xmm0
+    xorps sum, sum
+
+    .iLoop: ; for (i = 0; i < length; i+=4)
+    cmp i, length
+    jge .checkLast
+
+    movups xmm1, OWORD[x + i * FLOAT_LENGTH]
+    movups xmm2, OWORD[y + i * FLOAT_LENGTH] 
+    mulps xmm1, xmm2
+    addps sum, xmm1
+
+    add i, 4
+    jmp .iLoop
+
+    .checkLast:
+    cmp i, length
+    je .end
+    
+    movd xmm1, DWORD[x + i * FLOAT_LENGTH]
+    movd xmm2, DWORD[y + i * FLOAT_LENGTH]
+    mulps xmm1, xmm2
+    addps sum, xmm1
+
+    add i, 1
+    jmp .checkLast
+
+    .end:
+    haddps xmm0, xmm0
+    haddps xmm0, xmm0
+
+    ret
+
+global vectorf_smul_r_asm
+; void vectorf_mul_r_asm(float *vec(rdi), scalar(xmm0), unsigned int length(rsi))
+vectorf_smul_r_asm:
+    ; ----------------
+    ; ------Body------
+    ; ----------------
+    %define vec rdi
+    %define scalar, xmm0
+                               ; xmm0 = | ...    | ...    | ...    | scalar |    
+    shufps scalar, scalar, 0x0 ; xmm0 = | scalar | scalar | scalar | scalar |
+    
+    %define length r8
+    mov length, rsi
+    sub length, 3
+
+    %define i rcx
+    mov i, 0
+
+    .iLoop: ; for (i = 0; i < length; i+=4)
+    cmp i, length
+    jge .checkLast
+
+    movups xmm1, OWORD[vec + i * FLOAT_LENGTH] ; xmm1 = | vec[i+3]         | vec[i+2]          | vec[i+1]          | vec[i]          |
+    mulps xmm1, scalar                         ; xmm1 = | vec[i+3] * scalar| vec[i+2] * scalar | vec[i+1] * scalar | vec[i] * scalar |
+    movups OWORD[vec + i * FLOAT_LENGTH], xmm1
+
+    add i, 4
+    jmp .iloop
+
+    .checkLast:
+    cmp i, rdx
+    je .end
+
+    movd xmm1, DWORD[vec + i * FLOAT_LENGTH] ; xmm1 = | 0 | 0 | 0 | vec[i]          |
+    mulps xmm1, scalar                       ; xmm1 = | 0 | 0 | 0 | vec[i] * scalar |
+    movups DWORD[vec + i * FLOAT_LENGTH], xmm1
+
+    add i, 1
+    jmp .checkLast
+
+    .end:
+    ret
+
