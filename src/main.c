135ec39847e470940d04f4ce0bd0a3db6d3d9ba4
AvalosM <manuel.avalos.98@gmail.com>
2022-05-20 00:46:52 -0300

    Clean main

--- src/main.c
+++ src/main.c
@@ -1,14 +1,3 @@
-#include "io.h"
-#include "features.h"
-
 int main(void)
 {
-    // char *file_in = "data/OSR_us_000_0010_8k.wav";
-    // SF_INFO audio_info;
-    // float *signal = readwav(file_in, &audio_info);
-
-    // float *signal_features = features(signal, audio_info.frames, audio_info.samplerate);
-    
-    // char *file_out = "res/OSR_us_features.txt";
-    // savesignalf(file_out, signal_features, )
 }
