dd729404470137688dcd19926e99aa135f7f0e6d
AvalosM <manuel.avalos.98@gmail.com>
2022-05-20 00:46:43 -0300

    Change MEL_FILTER_NUM to 26

--- src/filters.h
+++ src/filters.h
@@ -4,7 +4,7 @@
 #include <math.h>
 #include "matrix.h"
 #define PI 3.14159265358979323846
-#define MEL_FILTER_NUM 40
+#define MEL_FILTER_NUM 26
 
 /**
  * @brief Pre-emphasis filter: Y[n] = X[n] - 0.95 * X[n - 1] where X is the input signal and Y the output
