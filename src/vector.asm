731ff747fbb1184c84d19a3aab7c6e7b84fe8d8c
AvalosM <manuel.avalos.98@gmail.com>
2022-05-21 00:47:39 -0300

    vectorf_norm_asm implementation

--- src/vector.asm
+++ src/vector.asm
@@ -5,8 +5,13 @@ extern fcmul_pair_asm
 
 section .text
 
-global vectorf_dot_asm
+;-------------------------------------------------------------------------------
+; Float vector dot product
+;
+; C signature:
 ; float vectorf_dot_asm(float *v1(rdi), float *v2(rsi), unsigned int length(rdx))
+;-------------------------------------------------------------------------------
+global vectorf_dot_asm
 vectorf_dot_asm:
     ; ----------------
     ; ------Body------
@@ -31,7 +36,7 @@ vectorf_dot_asm:
     movups xmm1, OWORD[x + i * FLOAT_LENGTH] ; xmm1 = | v1[i+3]                 | v1[i+2]                 | v1[i+1]                 | v1[i]               |
     movups xmm2, OWORD[y + i * FLOAT_LENGTH] ; xmm2 = | v2[i+3]                 | v2[i+2]                 | v2[i+1]                 | v2[i]               |
     mulps xmm1, xmm2                         ; xmm1 = | v2[i+3]                 | v2[i+2]                 | v2[i+1]                 | v2[i]               |
-    addps sum, xmm1                          ; xmm0 = | sun + v1[i+3] * v2[i+3] | sum + v1[i+2] * v2[i+2] | sum + v1[i+1] * v2[i+1] | sum + v1[i] * v2[i] |
+    addps sum, xmm1                          ; xmm0 = | sum + v1[i+3] * v2[i+3] | sum + v1[i+2] * v2[i+2] | sum + v1[i+1] * v2[i+1] | sum + v1[i] * v2[i] |
 
     add i, 4
     jmp .iLoop
@@ -54,8 +59,13 @@ vectorf_dot_asm:
 
     ret
 
-global vectorf_mul_r_asm
+;-------------------------------------------------------------------------------
+; Float vector in place entrywise multiplication
+;
+; C signature:
 ; void vectorf_mul_r_asm(float *v1(rdi), float *v2(rsi), unsigned int length(rdx))
+;-------------------------------------------------------------------------------
+global vectorf_mul_r_asm
 vectorf_mul_r_asm:
     ; ----------------
     ; ------Body------
@@ -97,8 +107,13 @@ vectorf_mul_r_asm:
    .end:
     ret 
 
-global vectorf_smul_r_asm
+;-------------------------------------------------------------------------------
+; Float vector in place multiplication by scalar
+;
+; C signature:
 ; void vectorf_mul_r_asm(float *vec(rdi), scalar(xmm0), unsigned int length(rsi))
+;-------------------------------------------------------------------------------
+global vectorf_smul_r_asm
 vectorf_smul_r_asm:
     ; ----------------
     ; ------Body------
@@ -138,4 +153,55 @@ vectorf_smul_r_asm:
     jmp .checkLast
 
     .end:
+    ret
+
+;-------------------------------------------------------------------------------
+; FLoat vector norm2
+;
+; C signature:
+; extern float vectorf_norm_asm(float *vec(rdi), unsigned int length(rsi))
+;-------------------------------------------------------------------------------
+global vectorf_norm_asm
+vectorf_norm_asm:
+    ; ----------------
+    ; ------Body------
+    ; ----------------
+    %define vec rdi
+    %define sum xmm0
+    xorps sum, sum
+
+    %define length r8
+    mov length, rsi
+    sub length, 3
+
+    %define i rcx
+    mov i, 0
+
+    .iLoop: ; for (i = 0; i < length; i+=4)
+    cmp i, length
+    jge .checkLast
+
+    movups xmm1, OWORD[vec + i * FLOAT_LENGTH] ; xmm1 = | vec[i+3]          | vec[i+2]          | vec[i+1]          | vec[i]          |
+    mulps xmm1, xmm1                           ; xmm1 = | vec[i+3]^2        | vec[i+2]^2        | vec[i+1]^2        | vec[i]^2        |
+    addps xmm0, xmm1                           ; xmm0 = | sum3 + vec[i+3]^2 | sum2 + vec[i+2]^2 | sum1 + vec[i+1]^2 | sum0 + vec[i]^2 |
+
+    add i, 4
+    jmp .iLoop
+
+    .checkLast:
+    cmp i, rsi
+    je .end
+
+    movd xmm1, DWORD[vec + i * FLOAT_LENGTH] ; xmm1 = | 0    | 0    | 0    | v1[i]          |
+    mulps xmm1, xmm1                         ; xmm1 = | 0    | 0    | 0    | v1[i]^2        |
+    addps xmm0, xmm1                         ; xmm0 = | sum3 | sum2 | sum1 | sum0 + v1[i]^2 |
+
+    add i, 1
+    jmp .checkLast
+
+    .end:
+    haddps xmm0, xmm0
+    haddps xmm0, xmm0
+    sqrtps xmm0, xmm0
+
     ret
