f4cf64579a6a095a2ce21672cf6e228dcb31d4a3
AvalosM <manuel.avalos.98@gmail.com>
2022-05-21 00:51:30 -0300

    Remove fcomplex asm implementations

--- src/fcomplex.c
+++ src/fcomplex.c
@@ -2,15 +2,7 @@
 
 fcomplex fcmul(fcomplex x, fcomplex y)
 {
-    #ifdef __FCOMPLEX_SSE__
-
-    return fcmul_pair_asm(x, y);
-
-    #else
-
     return (fcomplex){ x.real * y.real - x.imag * y.imag, x.real * y.imag + x.imag * y.real };
-
-    #endif
 }
 
 fcomplex fcadd(fcomplex x, fcomplex y)
@@ -25,13 +17,5 @@ fcomplex fcsub(fcomplex x, fcomplex y)
 
 float fcabs(fcomplex x)
 {
-    #ifdef __FCOMPLEX_SSE__
-
-    return fcabs_pair_asm(x);
-
-    #else
-
     return (float)sqrtf(powf(x.real, 2) + powf(x.imag, 2));
-
-    #endif
 }
