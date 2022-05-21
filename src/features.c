74fc93a01601daacb5835f18f2f09027f3e8cf0f
AvalosM <manuel.avalos.98@gmail.com>
2022-05-21 00:50:48 -0300

    Add features function

--- src/features.c
+++ src/features.c
@@ -1,6 +1,7 @@
 #include "features.h"
 #include "transforms.h"
 #include "filters.h"
+#include "float.h"
 
 matrixfc *frame(float *signal, unsigned int signal_length, unsigned int samplerate)
 {
@@ -35,6 +36,7 @@ matrixfc *frame(float *signal, unsigned int signal_length, unsigned int samplera
 
 matrixf *melspectogram(float *signal, unsigned int signal_length, unsigned int samplerate)
 {
+    preemphasis(signal, signal_length);
     matrixfc *frames = frame(signal, signal_length, samplerate);
     matrixf *filterbank = melfilterbank(MEL_LOWER_FREQ, samplerate / 2, samplerate, frames->cols);
 
@@ -48,7 +50,7 @@ matrixf *melspectogram(float *signal, unsigned int signal_length, unsigned int s
         fcomplex *frames_row_i = matrixfc_at(frames, i, 0);
         fft(frames_row_i, workspace, frames->cols);
     }
-
+    
     /* Power spectrum of each FFT(frame), |FFT(x_i)|^2 / N */
     matrixf *power_spectrums = matrixfc_abs(frames);          /* |FFT(frame_i)| */
     matrixf_mul_r(power_spectrums, power_spectrums);          /* |FFT(frame_i)|^2 */
@@ -58,31 +60,44 @@ matrixf *melspectogram(float *signal, unsigned int signal_length, unsigned int s
     /* Take the log of each value */
     for (unsigned int i = 0; i < spectogram->rows; i++) {
         for (unsigned int j = 0; j < spectogram->cols; j++) {
-            *matrixf_at(spectogram, i, j) = logf(*matrixf_at(spectogram, i, j));
+            float value = *matrixf_at(spectogram, i, j);
+            if (value == 0) value = FLT_MIN; /* Avoid ln(0) */
+            *matrixf_at(spectogram, i, j) = floorf(logf(*matrixf_at(spectogram, i, j)));
         }
     }
 
+    /* Cleanup */
+    free(workspace);
     matrixfc_free(frames);
     matrixf_free(filterbank);
-    free(workspace);
+    
     matrixf_free(power_spectrums);
 
     return spectogram;
 }
 
+
 matrixf *mfcc(float *signal, unsigned int signal_length, unsigned int samplerate)
 {
     matrixf *spectogram = melspectogram(signal, signal_length, samplerate);
+
+    /* Initialize lifter */
+    float *lifter = malloc(MEL_FILTER_NUM * sizeof(float));
+    for (unsigned int i = 0; i < MEL_FILTER_NUM; i++) {
+        lifter[i] = 1 + (CEP_LIFTER / 2) * sinf(PI * i / CEP_LIFTER);
+    }
+
     /* Apply FCT to each coefficient vector */
     matrixf *mfcc_matrix = matrixf_new(spectogram->rows, MFCC_FEATURE_NUM, ROW_MAJOR);
     for (unsigned int i = 0; i < spectogram->rows; i++) {
         float *spectogram_row_i = matrixf_at(spectogram, i, 0);
         fct(spectogram_row_i, spectogram->cols);
         /* Copy values */
-        for (unsigned int j = 0; j < MFCC_FEATURE_NUM; j++) {
-            *matrixf_at(mfcc_matrix, i, j) = spectogram_row_i[j];
+        for (unsigned int j = 1; j < MFCC_FEATURE_NUM + 1; j++) {
+            *matrixf_at(mfcc_matrix, i, j-1) = spectogram_row_i[j] * lifter[j-1];
         }
     }
+    // free(lift);
     matrixf_free(spectogram);
     return mfcc_matrix;
 }
--- src/features.h
+++ src/features.h
@@ -1,24 +1,27 @@
 #ifndef __FEATURES_H__
 #define __FEATURES_H__
 
+#include "config.h"
 #include "fcomplex.h"
 #include "matrix.h"
 
 #define FRAME_LEN_MS 0.025
 #define FRAME_STR_MS 0.01
 
-#define MEL_LOWER_FREQ 300
+#define MEL_LOWER_FREQ 64
 
 #define MFCC_FEATURE_NUM 12
 #define MFCC_FIRST_FEATURE 2
+#define CEP_LIFTER 22
 
 #define FEATURE_NUM 12
-
-
-matrixf *mfcc(float *signal, unsigned int signal_length, unsigned int samplerate);
 
 matrixfc *frame(float *signal, unsigned int signal_length, unsigned int samplerate);
 
 matrixf *melspectogram(float *signal, unsigned int signal_length, unsigned int samplerate);
+
+matrixf *mfcc(float *signal, unsigned int signal_length, unsigned int samplerate);
+
+matrixf *features(matrixf *features);
 
 #endif /* __FEATURES_H__ */
