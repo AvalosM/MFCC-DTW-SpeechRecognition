acf2c98d6423f19206a3af8a50a1f97f7321a567
AvalosM <manuel.avalos.98@gmail.com>
2022-09-20 15:41:53 -0300

    Refactor matrix functions
    Now using vector implementations

--- src/matrix.c
+++ src/matrix.c
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
-/** 
- * Float Matrix
+/** -------------------------------------------------------------------------------
+ *  Float Matrix
+ *  -------------------------------------------------------------------------------
  */
 
 matrixf *matrixf_new(unsigned int n_rows, unsigned int n_cols, int order)
@@ -20,6 +21,7 @@ matrixf *matrixf_new(unsigned int n_rows, unsigned int n_cols, int order)
     }
     mat->rows = n_rows;
     mat->cols = n_cols;
+    mat->size = n_rows * n_cols;
     mat->order = order;
 
     return mat;
@@ -61,61 +63,36 @@ matrixf *matrixf_dot_fast(matrixf *m1, matrixf *m2, unsigned int order)
         float *m1_row_i = matrixf_at(m1, i, 0);
         for (unsigned int j = 0; j < m2->cols; j++) {
             float *m2_col_j = matrixf_at(m2, 0, j);
-            float scalar = 0;
-            #ifdef __MATRIX_SSE__
-
-            scalar = vectorf_dot_asm(m1_row_i, m2_col_j, m1->cols);
-
-            #else
-
-            for (unsigned int k = 0; k < m1->cols; k++) {
-                scalar += m1_row_i[k] * m2_col_j[k];
-            }
-
-            #endif
-
-            *matrixf_at(res, i, j) = scalar;
+            *matrixf_at(res, i, j) = vectorf_dot(m1_row_i, m2_col_j, m1->cols);
         }
     }
     return res;
 }
 
-void matrixf_mul_r(matrixf *m1, matrixf *m2)
+void matrixf_mul(matrixf *m1, matrixf *m2, matrixf *result)
 {
     /* Check dimensions */
-    if (m1->rows != m2->rows || m1->cols != m2->cols) {
+    if (m1->rows != m2->rows || m1->cols != m2->cols || m1->size != result->size) {
         exit(-1);
     }
-    #ifdef __MATRIX_SSE__
 
-    vectorf_mul_asm(m1->data, m2->data, m1->data, m1->cols * m1->rows);
-
-    #else
-
-    for (unsigned int i = 0; i < m1->cols * m1->rows; i++) {
-        m1->data[i] = m1->data[i] * m2->data[i];
-    }
-
-    #endif
+    vectorf_mul(m1->data, m2->data, result->data, result->size);
 }
 
-void matrixf_smul_r(matrixf *mat, float scalar)
+void matrixf_smul(matrixf *mat, float scalar, matrixf *result)
 {
-    #ifdef __MATRIX_SSE__
-
-    vectorf_smul_asm(mat->data, scalar, mat->data, mat->cols * mat->rows);
-
-    #else
-    for (unsigned int i = 0; i < mat->cols * mat->rows; i++) {
-        mat->data[j] *= scalar;
+    /* Check result matrix is big enough */
+    if (mat->size != result->size) {
+        exit(-1);
     }
-    
-    #endif
+
+    vectorf_smul(mat->data, scalar, result->data, result->size);
 }
 
-/**
- * Float complex matrix 
- */
+/** -------------------------------------------------------------------------------
+ *  Float complex matrix 
+ *  -------------------------------------------------------------------------------
+ */ 
 
 matrixfc *matrixfc_new(unsigned int n_rows, unsigned int n_cols, int order)
 {
@@ -127,6 +104,7 @@ matrixfc *matrixfc_new(unsigned int n_rows, unsigned int n_cols, int order)
     mat->data = (fcomplex*)calloc(n_rows * n_cols, sizeof(fcomplex));
     mat->rows = n_rows;
     mat->cols = n_cols;
+    mat->size = n_rows * n_cols;
     mat->order = order;
 
     return mat;
@@ -146,43 +124,4 @@ fcomplex *matrixfc_at(matrixfc *mat, unsigned int row, unsigned int col)
     }
 
     return mat->order == ROW_MAJOR ? mat->data + (row * mat->cols + col) : mat->data + (col * mat->rows + row);   
-}
-
-void matrixfc_reshape(matrixfc *mat, unsigned int rows, unsigned int cols)
-{
-    /* Check new shape is within bounds*/
-    if (rows > mat->rows || cols > mat->cols) {
-        exit(-1);
-    }
-    if (mat->order == ROW_MAJOR) {
-        for (unsigned int i = 0; i < rows; i++) {
-            fcomplex *row_i = matrixfc_at(mat, i, 0);
-            memmove(mat->data + (i * cols), row_i, sizeof(fcomplex) * cols);
-        }
-    } else {
-        for (unsigned int j = 0; j < cols; j++) {
-            fcomplex *col_i = matrixfc_at(mat, 0, j);
-            memmove(mat->data + (j * rows), col_i, sizeof(fcomplex) * rows);
-        }
-    }
-
-    fcomplex *data_r = realloc(mat->data, sizeof(fcomplex) * rows * cols);
-    if(data_r) mat->data = data_r;
-
-    mat->rows = rows;
-    mat->cols = cols;
-}
-
-matrixf *matrixfc_abs(matrixfc *mat)
-{
-    matrixf *res = matrixf_new(mat->rows, mat->cols, ROW_MAJOR);
-
-    for (unsigned int i = 0; i < mat->rows; i++) {
-        fcomplex *mat_row_i = matrixfc_at(mat, i, 0);
-        float *res_row_i = matrixf_at(res, i, 0);
-        for (unsigned int j = 0; j < mat->cols; j++) {
-            res_row_i[j] = fcabs(mat_row_i[j]);
-        }
-    }
-    return res;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -1,7 +1,6 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
 
-#include "config.h"
 #include "fcomplex.h"
 
 #define ROW_MAJOR 1
@@ -14,6 +13,7 @@ typedef struct matrixf {
     float *data;
     unsigned int rows;
     unsigned int cols;
+    unsigned int size; /* rows * cols */
     int order;
 } matrixf;
 
@@ -21,15 +21,13 @@ matrixf *matrixf_new(unsigned int n_rows, unsigned int n_cols, int order);
 
 void matrixf_free(matrixf *mat);
 
-void matrixf_set(matrixf *mat, float value);
-
 float *matrixf_at(matrixf *mat, unsigned int row, unsigned int col);
 
 /**
  * @brief Float matrix fast dot product
  * 
  * Requires m1 to be row-major and m2 column-major.
- * Rows in m1 and columns in m2 being contiguous in memory allows for faster computation
+ * Rows in m1 and columns in m2 being contiguous in memory allows for faster computation.
  * Allocates matrix user should free
  * 
  * @param m1 Row-major float matrix
@@ -38,27 +36,9 @@ float *matrixf_at(matrixf *mat, unsigned int row, unsigned int col);
  */
 matrixf *matrixf_dot_fast(matrixf *m1, matrixf *m2, unsigned int order);
 
-/**
- * @brief Entrywise matrix multiplication
- * 
- * Matrices must have the same dimension
- * 
- * @param m1
- * @param m2
- */
-void matrixf_mul_r(matrixf *m1, matrixf *m2);
+void matrixf_mul(matrixf *m1, matrixf *m2, matrixf *result);
 
-/**
- * @brief In place Matrix multiplication by scalar
- * 
- * @param mat
- * @param scalar 
- */
-void matrixf_smul_r(matrixf *mat, float scalar);
-
-float vectorf_dist(float *v1, float *v2, unsigned int length);
-
-void matrixf_print(matrixf *mat);
+void matrixf_smul(matrixf *mat, float scalar, matrixf *result);
 
 /**
  * fcomplex matrix
@@ -67,6 +47,7 @@ typedef struct matrixfc {
     fcomplex *data;
     unsigned int rows;
     unsigned int cols;
+    unsigned int size; /* rows * cols */
     int order;
 } matrixfc;
 
@@ -77,19 +58,5 @@ void matrixfc_free(matrixfc *mat);
 void matrixfc_set(matrixfc *mat, fcomplex value);
 
 fcomplex *matrixfc_at(matrixfc *mat, unsigned int row, unsigned int col);
-
-void matrixfc_reshape(matrixfc *mat, unsigned int rows, unsigned int cols);
-
-/**
- * @brief Entrywise absolute value of an fcomplex matrix
- * 
- * Allocates matrix user shoud free
- * 
- * @param mat 
- * @return matrixf* Pointer to float matrix containing absolute values 
- */
-matrixf *matrixfc_abs(matrixfc *mat);
-
-void matrixfc_print(matrixfc *mat);
 
 #endif /* __MATRIX_H__ */
