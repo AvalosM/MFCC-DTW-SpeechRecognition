f37d7f4ed15a21377c11045eb365f5f19bbcf91d
AvalosM <manuel.avalos.98@gmail.com>
2022-09-20 15:55:38 -0300

    Refactor fftstockham, twiddle_init
    Using vector implementations

--- src/transforms.c
+++ src/transforms.c
@@ -2,69 +2,26 @@
 #include "vector.h"
 #include <string.h>
 
-fcomplex *twiddle = NULL;
-
-void twiddle_init(unsigned int size)
+fcomplex *twiddle_factors(unsigned int fft_size)
 {
-    static unsigned int init_size = 0;
-    
-    if (init_size != size) {
-        free(twiddle);
-        twiddle = (fcomplex*)malloc(size * sizeof(fcomplex));
-        if (!twiddle) {
-        exit(-1);
-        }
+    fcomplex *twiddle = malloc(fft_size * sizeof(fcomplex));
+    if (!twiddle) exit(-1);
 
-        for (unsigned int q = 0; (unsigned int)1 << q <= size; q++) {
-            unsigned int L = 1 << q;  /* L = 2^q; */
-            unsigned int Ls = L >> 1; /* Ls = L / 2 */
-            for (unsigned int j = 0; j < Ls; j++) {
-                twiddle[Ls - 1 + j].real = cosf(2 * PI * j / L);
-                twiddle[Ls - 1 + j].imag = -sinf(2 * PI * j / L);
-            }
-        }
-    }
-}
-
-void fftstockham(fcomplex *x, fcomplex *workspace, unsigned int N)
-{
-    fcomplex *y_ini = workspace;
-    /* For q=1 to log_2(N) */
-    for (unsigned int L = 2; L <= N; L <<= 1) {
-        
-        /* Swap pointers */
-        fcomplex *tmp = x;
-        x = workspace;
-        workspace = tmp;
-
-        unsigned int Ls = L >> 1;
-        unsigned int r = N / L;
-        unsigned int rs = N / Ls;
-
+    for (unsigned int q = 0; (unsigned int)1 << q <= fft_size; q++) {
+        unsigned int L = 1 << q;  /* L = 2^q; */
+        unsigned int Ls = L >> 1; /* Ls = L / 2 */
         for (unsigned int j = 0; j < Ls; j++) {
-            /* Twiddle factor */
-            fcomplex w = twiddle[Ls - 1 + j];
-            
-            for (unsigned int k = 0; k < r; k++) {
-                fcomplex t = fcmul(w, workspace[j * rs + k + r]);
-                x[(j * r) + k] = fcadd(workspace[(j * rs) + k], t);
-                x[((j + Ls) * r) + k] = fcsub(workspace[(j * rs) + k], t);
-            }
+            twiddle[Ls - 1 + j].real = cosf(2 * PI * j / L);
+            twiddle[Ls - 1 + j].imag = -sinf(2 * PI * j / L);
         }
     }
-    /* If log_2(N) is odd, result values are stored on additional workspace */
-    if (workspace != y_ini) {
-        for (unsigned int i = 0; i < N; i++) {
-            workspace[i] = x[i];
-        }
-    }
+
+    return twiddle;
 }
 
-void fftstockham_avx(fcomplex *x, fcomplex *y, unsigned int n)
+void fft(fcomplex *x, fcomplex *y, fcomplex *twiddle, unsigned int n)
 {
     fcomplex *y_ini = y;
-    /* Allocate memory for twiddle vector */
-    fcomplex *u = malloc((n >> 1) * sizeof(fcomplex));
 
     /* For q=1 to log_2(n) */
     for (unsigned int q = 1; (unsigned int)1 << q <= n; q++) {
@@ -78,9 +35,9 @@ void fftstockham_avx(fcomplex *x, fcomplex *y, unsigned int n)
         unsigned int r = n >> q;  /* r = n / L */
 
         for (unsigned int k = 0; k < r; k++) {
-            vectorfc_mul_asm(twiddle + Ls - 1, y + ((k + r) * Ls), u, Ls);
-            vectorfc_add_asm(y + k * Ls, u, x + k * L, Ls);
-            vectorfc_sub_asm(y + k * Ls, u, x + (k * L + Ls), Ls);
+            vectorfc_mul(twiddle + Ls - 1, y + ((k + r) * Ls), y + ((k + r) * Ls), Ls);
+            vectorfc_add(y + k * Ls, y + ((k + r) * Ls), x + k * L, Ls);
+            vectorfc_sub(y + k * Ls, y + ((k + r) * Ls), x + (k * L + Ls), Ls);
         }
     }
     /* If log_2(N) is odd, result values are stored on additional workspace */
@@ -89,16 +46,6 @@ void fftstockham_avx(fcomplex *x, fcomplex *y, unsigned int n)
             y[i] = x[i];
         }
     }
-    free(u);
-}
-
-void fft(fcomplex *x, fcomplex *workspace, unsigned int N)
-{
-    #ifdef __FFT_AVX__
-        fftstockham_avx(x, workspace, N);
-    #else
-        fftstockham(x, workspace, N);
-    #endif
 }
 
 /* Naive Discrete cosine transform II implementation */
--- src/transforms.h
+++ src/transforms.h
@@ -1,39 +1,38 @@
 #ifndef __TRANSFORMS_H__
 #define __TRANSFORMS_H__
 
-#include "config.h"
 #include <stdlib.h>
 #include <math.h>
 #include "fcomplex.h"
 
 #define PI 3.14159265358979323846
-#define TWIDDLE_TABLE_SIZE 1024
-
-
-extern fcomplex *twiddle;
 
 /**
- * @brief Init twiddle factor table
+ * @brief Calculate twiddle factors to perform fft.
  * 
- * Precalculate twiddle factors from e^(-2PIi * 0 / FFT_POINT_MAX) to e^(-2PIi * TWIDDLE_TABLE_SIZE / FFT_POINT_MAX)
+ * Allocates memory user should free.
+ * 
+ * @param fft_size 
+ * @return fcomplex* 
  */
-void twiddle_init();
+fcomplex *twiddle_factors(unsigned int fft_size);
 
 /**
  * @brief Stockham decimation in time Fast Fourier Transform.
  * 
- * Implementation depends on definition of __FFT_SSE__.
- *
- * Result of the transform is stored in input signal array, signal length(N) MUST be a power of 2 and <= 2 * TWIDDLE_TABLE_SIZE.
+ * Result of the transform is stored in input signal array.
  * 
  * Reference:
  * VAN LOAN, Charles. Computational frameworks for the fast Fourier transform. Society for Industrial and Applied Mathematics, 1992. p.57
  * 
- * @param x Input signal
- * @param workspace Additional workspace (size N) required to compute the transform
- * @param N Signal length 
+ * @param signal
+ * @param workspace Additional workspace (size signal_length) required to compute the transform
+ * @param twiddle Precalculated twiddle factors
+ * @param signal_length
  */
-void fft(fcomplex *signal, fcomplex *workspace, unsigned int signal_length);
+void fft(fcomplex *signal, fcomplex *workspace, fcomplex *twiddle, unsigned int signal_length);
+
+
 
 /**
  * @brief Fast cosine transform using forward fft
@@ -44,9 +43,9 @@ void fft(fcomplex *signal, fcomplex *workspace, unsigned int signal_length);
  * MAKHOUL, John. A fast cosine transform in one and two dimensions. IEEE Transactions on Acoustics, Speech, and Signal Processing, 1980, vol. 28, no 1, p. 27-34.
  * https://www.nayuki.io/page/fast-discrete-cosine-transform-algorithms
  * 
- * @param x Input signal
+ * @param signal
  * @param workspace Additional workspace (size N) required to compute the transform
- * @param N Signal length
+ * @param signal_length
  */
 void fct(float *signal, unsigned int signal_length);
 
