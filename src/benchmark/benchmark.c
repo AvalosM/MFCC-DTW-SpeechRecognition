5311630c463ef52b8115fe006379b969ff6c9aac
AvalosM <manuel.avalos.98@gmail.com>
2022-09-20 15:58:51 -0300

    File cleanup

--- src/benchmark/benchmark.c
+++
@@ -1,7 +0,0 @@
-#include "benchmark.h"
-
-int main(void)
-{
-    fft_benchmark();
-    return 0;
-}
--- src/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef __CONFIG_H__
-#define __CONFIG_H__
-
-#define __MATRIX_SSE__
-#define __FFT_AVX__
-
-#endif /* __CONFIG_H__ */
